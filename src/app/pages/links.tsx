45a79657f533cc195c1dd553826c9135f067b7ef
David Fadeyi <fadav4real@gmail.com>
2025-09-08 19:12:49 +0100

    Update links.tsx

--- src/app/pages/links.tsx
+++ src/app/pages/links.tsx
@@ -8,31 +8,20 @@ export const links = [
   {
     index: 0,
     title: "Find me on Github",
-    href: "https://github.com/samadaderinto",
+    href: "https://github.com/jesudeyi",
     icon: <FaGithub />,
   },
   {
     index: 1,
     title: "Find me on LinkedIn",
-    href: "https://www.linkedin.com/in/samadaderinto/",
+    href: "https://www.linkedin.com/in/david-fadeyi/",
     icon: <FaLinkedin />,
   },
   {
     index: 2,
     title: "Contact me via email",
-    href: "mailto:adesamad1234@gmail.com",
+    href: "mailto:fadav4real@gmail.com",
     icon: <FaEnvelope />,
   },
-  // {
-  //   index: 3,
-  //   title: "Find me on Medium",
-  //   href: "https://medium.com/@noworneverev",
-  //   icon: <FaMedium />,
-  // },
-  // {
-  //   index: 4,
-  //   title: "Find me on Blog",
-  //   href: "https://noworneverev.github.io/blog/",
-  //   icon: <FaBlog />,
-  // },
+ 
 ];
